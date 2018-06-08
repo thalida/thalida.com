935cb68bea960e2a5de1560ae26be7b7e9293d52
Thalida Noel <thalida.c.noel@gmail.com>
2018-06-07 20:33:49 -0400

    Setup more tests

--- tests/test_markdown_posts.py
+++ tests/test_markdown_posts.py
@@ -14,7 +14,9 @@ class TestMarkdownPosts(unittest.TestCase):
                 'meta': {
                     'url': '/x/file1',
                     'title': 'a',
-                    'date': '1',
+                    'date': '2018-01-01T00:00:00',
+                    'is_draft': False,
+                    'is_hidden': False,
                     'is_visible': True,
                 },
             },
@@ -24,7 +26,9 @@ class TestMarkdownPosts(unittest.TestCase):
                 'meta': {
                     'url': '/x/file2',
                     'title': 'a',
-                    'date': '2',
+                    'date': '2018-01-02T00:00:00',
+                    'is_draft': False,
+                    'is_hidden': False,
                     'is_visible': True,
                 },
             },
@@ -34,7 +38,9 @@ class TestMarkdownPosts(unittest.TestCase):
                 'meta': {
                     'url': '/x/file3',
                     'title': 'b',
-                    'date': '2',
+                    'date': '2018-01-02T00:00:00',
+                    'is_draft': False,
+                    'is_hidden': False,
                     'is_visible': True,
                 },
             },
@@ -44,51 +50,92 @@ class TestMarkdownPosts(unittest.TestCase):
                 'meta': {
                     'url': '/x/nested/nested1',
                     'title': 'c',
-                    'date': '2',
+                    'date': '2018-01-05T00:00:00',
+                    'is_draft': True,
+                    'is_hidden': False,
+                    'is_visible': False,
+                },
+            },
+            {
+                'file': 'posts/nested/nested2.md',
+                'html': 'd',
+                'meta': {
+                    'url': '/x/nested/nested2',
+                    'title': 'd',
+                    'date': '2007-09-16T00:00:00',
+                    'is_draft': False,
+                    'is_hidden': True,
                     'is_visible': False,
                 },
             },
         ]
 
-        self.provide_posts = {v['file']: {'html': v['html'], 'meta': v['meta']} for v in posts}
-        self.provide_visible_posts = {k:v for k,v in self.provide_posts.items() if v['meta']['is_visible'] is True}
-        self.provide_visible_meta = [self.provide_visible_posts[k]['meta'] for k in self.provide_visible_posts]
-        self.provide_visible_meta_by_date = [
-            self.provide_visible_meta[2],
-            self.provide_visible_meta[1],
-            self.provide_visible_meta[0],
-        ]
-        self.total_posts = len(self.provide_posts.keys())
-        self.total_visible_posts = len(self.provide_visible_posts.keys())
+        self.expected_posts = {post['file']: {'html': post['html'], 'meta': post['meta']} for post in posts}
+        
 
     def test_posts_collection_after_load(self):
         markdown = MarkdownPosts(run_load=False)
-        markdown._fetch_post_filepaths = MagicMock(return_value=self.provide_posts.keys())
-        markdown._fetch_post = MagicMock(side_effect=self.provide_posts.values())
+        markdown._fetch_post_filepaths = MagicMock(return_value=self.expected_posts.keys())
+        markdown._fetch_post = MagicMock(side_effect=self.expected_posts.values())
 
         markdown._load_posts()
-        posts = markdown.posts_collection
+        actual_posts = markdown.posts_collection
+
+        expected_visible_posts = {file:post for file,post in self.expected_posts.items() if post['meta']['is_visible'] is True}
+        expected_visible_meta = [expected_visible_posts[post]['meta'] for post in expected_visible_posts]
+        expected_visible_meta_by_date = [
+            expected_visible_meta[2],
+            expected_visible_meta[1],
+            expected_visible_meta[0],
+        ]
+        expected_total_posts = len(self.expected_posts.keys())
+        expected_total_visible_posts = len(expected_visible_posts.keys())
 
         assert markdown._fetch_post_filepaths.call_count == 1
-        assert markdown._fetch_post.call_count == self.total_posts
+        assert markdown._fetch_post.call_count == expected_total_posts
 
-        assert len(posts['all'].keys()) == self.total_posts
-        assert len(posts['visible'].keys()) == self.total_visible_posts
-        assert len(posts['visible_meta']) == self.total_visible_posts
-        assert len(posts['visible_meta_by_date']) == self.total_visible_posts
+        assert len(actual_posts['all'].keys()) == expected_total_posts
+        assert len(actual_posts['visible'].keys()) == expected_total_visible_posts
+        assert len(actual_posts['visible_meta']) == expected_total_visible_posts
+        assert len(actual_posts['visible_meta_by_date']) == expected_total_visible_posts
 
-        assert posts['all'].keys() == self.provide_posts.keys()
-        assert posts['visible'].keys() == self.provide_visible_posts.keys()
-        assert posts['visible_meta'] == self.provide_visible_meta
-        assert posts['visible_meta_by_date'] == self.provide_visible_meta_by_date
+        assert actual_posts['all'].keys() == self.expected_posts.keys()
+        assert actual_posts['visible'].keys() == expected_visible_posts.keys()
+        assert actual_posts['visible_meta'] == expected_visible_meta
+        assert actual_posts['visible_meta_by_date'] == expected_visible_meta_by_date
 
-        assert markdown.visible_meta == self.provide_visible_meta
-        assert markdown.visible_meta_by_date == self.provide_visible_meta_by_date
+        assert markdown.visible_meta == expected_visible_meta
+        assert markdown.visible_meta_by_date == expected_visible_meta_by_date
 
     def test_format_meta(self):
-        markdown = MarkdownPosts(run_load=False)
-        markdown._convert_path_to_url = MagicMock(side_effect=[v['meta']['url'] for k,v in self.provide_posts.items()])
+        unformatted_meta = { file: {k.title(): str(v) for k,v in post['meta'].items() if k is not 'is_visible'} for file,post in self.expected_posts.items()}
+        unformatted_meta['posts/nested/nested2.md'].pop('Date')
+        unformatted_meta['posts/nested/nested2.md'].pop('Is_Draft')
 
+        markdown = MarkdownPosts(run_load=False)
+
+        for path, post in self.expected_posts.items():
+            expected = post['meta']
+            actual = markdown._format_meta(unformatted_meta[path], path)
+            assert actual == expected
+
+    def test_convert_url_to_path(self):
+        markdown = MarkdownPosts(run_load=False)
+
+        for path, post in self.expected_posts.items():
+            actual = markdown._convert_url_to_path(post['meta']['url'])
+            expected = path
+
+            assert actual == expected
+
+    def test_convert_path_to_url(self):
+        markdown = MarkdownPosts(run_load=False)
+
+        for path, post in self.expected_posts.items():
+            actual = markdown._convert_path_to_url(path)
+            expected = post['meta']['url']
+
+            assert actual == expected
 
 if __name__ == '__main__':
     unittest.main()
