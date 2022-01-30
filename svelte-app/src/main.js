8be7e719ffe1209f699601e61a2dfafd0b55e235
Thalida Noel <thalida.c.noel@gmail.com>
2022-01-30 21:35:49 +0000

    Add fitty

--- svelte-app/src/main.js
+++ svelte-app/src/main.js
@@ -1,9 +1,21 @@
 import "./app.css";
 import "./styles/notion-theme.scss";
 import App from "./App.svelte";
+import fitty from "fitty";
 
 const app = new App({
   target: document.querySelector("#svelte-app"),
 });
+
+setTimeout(() => {
+  fitty('.notion-header__title', {
+    minSize: 32,
+    maxSize: 256,
+  });
+  fitty('h1.notion-heading .notion-semantic-string', {
+    minSize: 18,
+    maxSize: 128,
+  })
+}, 200);
 
 export default app;
