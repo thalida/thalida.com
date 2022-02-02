5641713de88c5411c07cd4dfa8f7763e8498e493
Thalida Noel <thalida.c.noel@gmail.com>
2022-02-02 04:09:50 +0000

    header style 3.3

--- svelte-app/src/main.js
+++ svelte-app/src/main.js
@@ -1,17 +1,5 @@
 import "./styles/notion-theme.scss";
 import App from "./App.svelte";
-import fitty from "fitty";
-
-// setTimeout(() => {
-//   fitty('.notion-header__title', {
-//     minSize: 32,
-//     maxSize: 128,
-//   });
-//   fitty('h1.notion-heading .notion-semantic-string', {
-//     minSize: 18,
-//     maxSize: 128,
-//   });
-// }, 200);
 
 const app = new App({
   target: document.querySelector("#svelte-app"),
