29f9c06cc317b1e6dae1bce89ca863207162f250
Thalida Noel <thalida.c.noel@gmail.com>
2022-02-24 04:19:04 +0000

    Added mobile nav

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -2,12 +2,13 @@
 	import LiveWindow from "./components/LiveWindow.svelte";
 	let scrollY = 0;
 
+	export let mobileNavOpen = false;
 	export let isHeaderSticky = false;
 	export const selectedPage = `/${window.location.pathname.split("/")[1]}`;
 	export const links = [
 		{ path: "/projects", label: "Projects" },
 		{ path: "/links", label: "Links" },
-		{ path: "/wiki", label: "Wiki" },
+		{ path: "/rubber-duck", label: "Rubber Duck" },
 		{ path: "/about", label: "About" },
 	];
 
@@ -28,7 +29,14 @@
 		<a class="notion-link link" class:active={selectedPage == "/"} href="/">
 			thalida.
 		</a>
-		<nav>
+
+		<button
+			class="nav-btn"
+			class:is-open={mobileNavOpen}
+			on:click={() => (mobileNavOpen = !mobileNavOpen)}
+		/>
+
+		<nav class:is-open={mobileNavOpen}>
 			{#each links as link}
 				<a class="notion-link link" href={link.path}>
 					{link.label}
@@ -53,6 +61,7 @@
 	}
 
 	header {
+		position: relative;
 		display: flex;
 		flex-flow: row nowrap;
 		align-items: center;
@@ -75,6 +84,58 @@
 			font-weight: bold;
 		}
 
+		.nav-btn {
+			position: relative;
+			display: none;
+			border: 0;
+			padding: 0;
+
+			&,
+			&:before,
+			&:after {
+				content: "";
+				display: block;
+				width: 24px;
+				height: 2px;
+				background: var(--color-text-default);
+			}
+
+			&:before,
+			&:after {
+				position: absolute;
+				left: 0;
+				transform: rotate(0);
+				transform-origin: center;
+				transition: all 200ms ease-in-out;
+			}
+
+			&:before {
+				top: -6px;
+			}
+
+			&:after {
+				top: 6px;
+			}
+
+			&.is-open {
+				background: transparent;
+				&:before,
+				&:after {
+					top: 0;
+				}
+				&:before {
+					transform: rotate(-45deg);
+				}
+				&:after {
+					transform: rotate(45deg);
+				}
+			}
+
+			@media (max-width: 500px) {
+				display: block;
+			}
+		}
+
 		nav {
 			display: flex;
 			flex-flow: row nowrap;
@@ -85,6 +146,34 @@
 				margin: 0 8px;
 				&:hover {
 					opacity: 1;
+				}
+			}
+
+			@media (max-width: 500px) {
+				display: flex;
+				flex-flow: column nowrap;
+				position: absolute;
+				justify-content: center;
+				position: absolute;
+				top: 64px;
+				right: 0;
+				width: 100%;
+				height: 0;
+				padding: 32px;
+				overflow: hidden;
+				visibility: hidden;
+				transition: all 200ms ease-in-out;
+				background: var(--color-bg-default);
+				box-shadow: 0 5px 30px var(--color-bg-default);
+				font-size: 18px;
+
+				&.is-open {
+					visibility: visible;
+					height: calc(95vh - 64px);
+				}
+
+				a {
+					margin: 16px 0;
 				}
 			}
 		}
