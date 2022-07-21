007e710e8782ed61022f458b55a8f34fce1ee482
Thalida Noel <thalida.c.noel@gmail.com>
2022-07-21 18:10:38 +0000

    nav

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -5,6 +5,17 @@
 
 	currentUrlPath.subscribe((urlPath) => {
 		selectedPage = `/${urlPath.split("/")[1]}`;
+
+		Array.from(
+			document.querySelectorAll(".notion-link.super-navbar__item")
+		).forEach((el) => {
+			const href = el.getAttribute("href");
+			el.classList.remove("active");
+
+			if (selectedPage.startsWith(href)) {
+				el.classList.add("active");
+			}
+		});
 	});
 </script>
 
