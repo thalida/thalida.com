6077535847424096a384f2e1f34dd97b5464ae40
Thalida Noel <thalida.c.noel@gmail.com>
2022-07-21 04:53:01 +0000

    main

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -2,6 +2,11 @@
 	import LiveWindow from "./components/LiveWindow.svelte";
 	export let selectedPage = `/${window.location.pathname.split("/")[1]}`;
 	console.log(selectedPage);
+
+	window.addEventListener("popstate", function (event) {
+		// Log the state data to the console
+		console.log(event.state);
+	});
 </script>
 
 {#if selectedPage === "/"}
