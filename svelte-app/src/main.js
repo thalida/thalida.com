008d5862e312bfb8d68ec51142ebed2bb315c11f
Thalida Noel <hello@thalida.com>
2023-04-29 00:41:44 +0000

    Cleanup for Svelete v3

--- svelte-app/src/main.js
+++ svelte-app/src/main.js
@@ -6,16 +6,8 @@ import "./styles/prism-themes/prism-shades-of-purple.css";
 import "./styles/app.scss";
 
 import LiveWindowApp from "./LiveWindowApp.svelte";
-import ThemeSwitcherApp from './ThemeSwitcherApp.svelte'
 
 export const liveWindowApp = new LiveWindowApp({
   target: document.querySelector(".super-content-wrapper"),
   anchor: document.querySelector(".super-content"),
 });
-
-export const themeSwitcherApp = new ThemeSwitcherApp({
-  target: document.querySelector(".super-navbar__content"),
-  anchor: document.querySelector(".super-navbar__logo.notion-link"),
-});
-
-// export default windowApp;
