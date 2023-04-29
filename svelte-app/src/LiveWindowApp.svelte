8d5f28421fdfcdab9c62c27e5449b35dc86a68c3
Thalida Noel <hello@thalida.com>
2023-04-29 17:07:41 +0000

    Updated window color

--- svelte-app/src/LiveWindowApp.svelte
+++ svelte-app/src/LiveWindowApp.svelte
@@ -20,12 +20,12 @@
 {/if}
 
 <style lang="scss" global>
-	.theme-dark {
+	html.theme-dark {
 		--window-color: var(--color-bg-default);
 		--window-clock-bg: var(--window-color);
 		--window-sky-color-default: var(--color-bg-default-darker);
 	}
-	.theme-light {
+	html.theme-light {
 		--window-color: #3e1300;
 		--window-clock-bg: #0e121b;
 		--window-sky-color-default: var(--color-bg-default-darker);
@@ -36,31 +36,4 @@
 			margin-top: 100px;
 		}
 	}
-
-	// @media (min-width: 980px) {
-	// 	.has-window {
-	// 		.scene {
-	// 			position: sticky;
-	// 			top: calc(var(--navbar-height) * 2);
-	// 		}
-
-	// 		.super-content-wrapper {
-	// 			display: flex;
-	// 			flex-flow: row wrap;
-	// 			max-width: var(--layout-max-width);
-	// 			margin: auto;
-	// 			align-items: start;
-	// 			justify-content: space-between;
-	// 			width: 100%;
-	// 			padding-left: var(--padding-left);
-	// 			padding-right: var(--padding-right);
-	// 		}
-
-	// 		.super-content {
-	// 			padding: 0 64px;
-	// 			flex-grow: 1;
-	// 			margin: calc(var(--navbar-height) - 1rem) 0 !important;
-	// 		}
-	// 	}
-	// }
 </style>
