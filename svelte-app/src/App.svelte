3c374a73937b62daae44da7b6c30ac55a4be1ea2
Thalida Noel <hello@thalida.com>
2023-11-11 22:02:57 +0000

    Fixed Prism

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -2,12 +2,30 @@
 	import { movePageProperties } from "./domManipulation";
 	import { currentUrlPath } from "./store";
 	import LiveWindow from "./components/LiveWindow.svelte";
+
 	import Prism from 'prismjs';
+	import 'prismjs/components/prism-bash';
+	import 'prismjs/components/prism-css';
+	import 'prismjs/components/prism-csv';
+	import 'prismjs/components/prism-docker';
+	import 'prismjs/components/prism-git';
+	import 'prismjs/components/prism-json';
+	import 'prismjs/components/prism-jsx';
+	import 'prismjs/components/prism-markdown';
+	import 'prismjs/components/prism-markup';
+	import 'prismjs/components/prism-mongodb';
+	import 'prismjs/components/prism-python';
+	import 'prismjs/components/prism-regex';
+	import 'prismjs/components/prism-sql';
+	import 'prismjs/components/prism-typescript';
+	import 'prismjs/components/prism-yaml';
+
 	export let selectedPage = null;
 	export let windowPage = "/";
 
 	currentUrlPath.subscribe((urlPath) => {
 		movePageProperties();
+
 		Prism.highlightAll();
 
 		selectedPage = `/${urlPath.split("/")[1]}`;
